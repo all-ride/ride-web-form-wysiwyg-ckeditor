f00efc995f0aa0ac9a903c40e88064395e7b3128
Joris Vandeweerd <joris@statik.be>
2014-04-18 13:10:13 +0200

    initial commit

---
+++ src/ride/library/form/row/CkeditorWysiwygRow.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace ride\library\form\row;
+
+/**
+ * Wysiwyg implementation for the CKEditor
+ */
+class CkeditorWysiwygRow extends WysiwygRow {
+
+    /**
+     * Gets all the javascript files which are needed for this row
+     * @return array|null
+     */
+    public function getJavascripts() {
+        return array(
+            '<script type="text/javascript">var CKEDITOR_BASEPATH = "' . $this->baseUrl . '/ckeditor/";</script>',
+            'ckeditor/ckeditor.js',
+        );
+    }
+
+    /**
+     * Gets all the inline javascripts which are needed for this row
+     * @return array|null
+    */
+    public function getInlineJavascripts() {
+        $properties = $this->properties;
+
+        $properties['baseHref'] = $this->baseUrl . '/';
+        $properties['language'] = $this->locale;
+
+        $json = json_encode($properties);
+
+        return array('CKEDITOR.replace("' . $this->widget->getId() . '", ' . $json . ');');
+    }
+
+}
